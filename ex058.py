b0b268b671b7fa1cfa6c9e79f56769cff7e7ccec
gersonnn172 <gersonbentovieira@gmail.com>
2026-08-17 13:34:25 -0300

    Add files via upload

---
+++ ex058.py
@@ -0,0 +1,18 @@
+from random import randint
+computador = randint (0, 10)
+print('Sou seu computador... Acabei de pensar em um número entre 0 e 10.')
+print("Será que voce consegue adivinhar qual foi? ")
+acertou = False
+palpites = 0
+while not acertou:
+    jogador = int(input('Qual é seu palpite? '))
+    palpites += 1
+    if jogador == computador:
+        acertou = True
+    else:
+        if jogador < computador:
+            print('MAIS... Tente novamente.')
+        elif jogador > computador:
+            print('MENOS... Tente novamente.')
+print('Acertou com {} tentativas'.format(palpites))
+
