91092b1d43226c2b4f6499ceb7bcbaa313490651
gersonnn172 <gersonbentovieira@gmail.com>
2026-08-17 13:24:16 -0300

    Add files via upload

---
+++ ex031.py
@@ -0,0 +1,8 @@
+distancia = float(input('Qual a distancia da sua viagem: '))
+print('Voce esta prestes a começar uma viagem de {}Km'.format(distancia))
+if distancia <= 200:
+    preço = distancia * 0.50
+else:
+    preço = distancia * 0.45
+print('O preço da sua passagem é R${:.2f}'.format(preço))
+
